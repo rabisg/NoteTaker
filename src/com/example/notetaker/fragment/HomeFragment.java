3c3206cdca39c27aeb99bd35be4e33a013432016
Rabi Shanker Guha <guha.rabishankar@gmail.com>
2014-06-30 13:12:41 +0530

    Remove separate fragment for ListView to simplify things

--- src/com/example/notetaker/fragment/HomeFragment.java
+++ src/com/example/notetaker/fragment/HomeFragment.java
@@ -1,6 +1,9 @@
 package com.example.notetaker.fragment;
 
+import java.util.Vector;
+
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,8 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.notetaker.NoteActivity;
@@ -25,6 +30,7 @@ public class HomeFragment extends Fragment implements OnClickListener {
 
 	Button newNoteButton;
 	EditText quickNoteText;
+	NotesListAdapter notesListAdapter;
 
 	public HomeFragment() {
 	}
@@ -35,15 +41,20 @@ public class HomeFragment extends Fragment implements OnClickListener {
 		View rootView = inflater.inflate(R.layout.fragment_home, container,
 				false);
 		newNoteButton = (Button) rootView.findViewById(R.id.newNoteButton);
-		quickNoteText = (EditText) rootView.findViewById(R.id.quick_note_text);
 		newNoteButton.setOnClickListener(this);
+
+		ListView notesList = (ListView) rootView.findViewById(R.id.notes_list);
+		notesListAdapter = new NotesListAdapter(getActivity());
+		notesList.setAdapter(notesListAdapter);
+
+		quickNoteText = (EditText) rootView.findViewById(R.id.quick_note_text);
 		quickNoteText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 
 			@Override
 			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
 				if(actionId == EditorInfo.IME_ACTION_DONE) {
 					// Add New Note
-					NotesListFragment.addNote(new Note("", quickNoteText.getText().toString()));
+					notesListAdapter.addNote(new Note("", quickNoteText.getText().toString()));
 					quickNoteText.setText("");
 					return true;
 				}
@@ -63,6 +74,46 @@ public class HomeFragment extends Fragment implements OnClickListener {
 			default:
 				Log.d("DEBUG", "Not Implemented");
 				break;
+		}
+	}
+
+	public class NotesListAdapter extends BaseAdapter {
+
+		private Context mContext;
+		private Vector<Note> notes = new Vector<Note>();
+
+		NotesListAdapter(Context context) {
+			mContext = context;
+		}
+		public void addNote(Note note) {
+			notes.add(note);
+			this.notifyDataSetChanged();
+		}
+		@Override
+		public int getCount() {
+			return notes.size();
+		}
+
+		@Override
+		public Object getItem(int pos) {
+			return notes.elementAt(pos);
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int pos, View view, ViewGroup viewGroup) {
+			if(view == null)
+				view = LayoutInflater.from(mContext).inflate(R.layout.list_item_note, null);
+
+			TextView titleTextView = (TextView)view.findViewById(R.id.item_note_title);
+			titleTextView.setText(notes.elementAt(pos).title);
+			TextView contentTextView = (TextView)view.findViewById(R.id.item_note_content);
+			contentTextView.setText(notes.elementAt(pos).content);
+			return view;
 		}
 	}
 }
